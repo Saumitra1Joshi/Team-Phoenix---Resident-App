95d401410a73fe175c0b330ace312a31891aa69e
Nisbij <42967110+Nisbij@users.noreply.github.com>
2020-09-19 00:54:09 +0530

    Create googleSignUp.js

---
+++ Routes/UserRoutes/googleSignUp.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const { check, validationResult } = require('express-validator');
+const Users = require('../../Modals/user');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+const router = express.Router();
+
+router.post(
+  '/',
+  [
+    check('googleID', 'Must include Google ID').not().isEmpty(),
+    check('email', 'enter a valid email').isEmail(),
+    check('username', 'enter Username').not().isEmpty(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { email, username, googleID } = req.body;
+
+    try {
+      var user = await Users.findOne({ googleID });
+      if (user) {
+        const payload = {
+          user: {
+            id: user.id,
+          },
+        };
+        jwt.sign(
+          payload,
+          config.get('jwtSecret'),
+          {
+            expiresIn: 1209600,
+          },
+          (err, token) => {
+            if (err) {
+              throw err;
+            } else {
+              console.log('user logged in ');
+              res.json({ token });
+            }
+          }
+        );
+      } else {
+        user = new Users({
+          name: username,
+          username: username,
+          email: email,
+          googleID: googleID,
+        });
+        await user.save();
+        const payload = {
+          user: {
+            id: user.id,
+          },
+        };
+        jwt.sign(
+          payload,
+          config.get('jwtSecret'),
+          {
+            expiresIn: 1209600,
+          },
+          (err, token) => {
+            if (err) {
+              throw err;
+            } else {
+              res.json({ token });
+            }
+          }
+        );
+      }
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send('Server error');
+    }
+  }
+);
+
+module.exports = router;
